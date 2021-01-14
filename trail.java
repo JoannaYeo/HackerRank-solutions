b7cf31abba26b93cba8dc68a0896ade8c9d6e87b
Joanna Yeo <shortmojo54321@hotmail.com>
2021-01-14 11:12:21 +1100

    Trial commit

---
+++ trail.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Person{
+
+    public static void main(String[] args){
+        String s = "hello";
+        int b = 9;
+        System.out.printf("%s%d\n%s",s,b,s);
+    }
+}
